29e56555062f7c645a09f575b58c38f214f8e82b
Mattbar <mlbarnes04@gmail.com>
2020-02-22 23:47:37 -0800

    clean up file

--- jsServer/server.js
+++ jsServer/server.js
@@ -181,7 +181,6 @@ app.get("/song", function(req, res) {
 });
 
 var server = app.listen(8081, function() {
-  var host = server.address().address;
   var port = server.address().port;
 
   console.log("Example app listening at", port);
